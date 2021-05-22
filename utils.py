472515efa5f867733aabced7d85b7317950aa8bb
benedettaliberatori <benny.libera@gmail.com>
2021-05-22 17:19:39 +0200

    accuracy

--- utils.py
+++ utils.py
@@ -70,6 +70,40 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
+    
+def class_accuracy(model, loader, threshold, device):
+    model.eval()
+    tot_class_preds, correct_class = 0, 0
+    tot_noobj, correct_noobj = 0, 0
+    tot_obj, correct_obj = 0, 0
+
+    for x, y in loader: 
+        x = x.to(device)
+        with torch.no_grad():
+            out = model(x)
+
+        for i in range(2):
+            y[i] = y[i].to(device)
+            obj = y[i][..., 0] == 1 # in paper this is Iobj_i
+            noobj = y[i][..., 0] == 0  # in paper this is Iobj_i
+
+            correct_class += torch.sum(
+                torch.argmax(out[i][..., 5:][obj], dim=-1) == y[i][..., 5][obj]
+            )
+            tot_class_preds += torch.sum(obj)
+
+            obj_preds = torch.sigmoid(out[i][..., 0]) > threshold
+            correct_obj += torch.sum(obj_preds[obj] == y[i][..., 0][obj])
+            tot_obj += torch.sum(obj)
+            correct_noobj += torch.sum(obj_preds[noobj] == y[i][..., 0][noobj])
+            tot_noobj += torch.sum(noobj)
+
+    print(f"Class accuracy is: {(correct_class/(tot_class_preds+1e-16))*100:2f}%")
+    print(f"No obj accuracy is: {(correct_noobj/(tot_noobj+1e-16))*100:2f}%")
+    print(f"Obj accuracy is: {(correct_obj/(tot_obj+1e-16))*100:2f}%")
+    model.train()
+
+
 def get_data(train_csv_path, test_csv_path):
     
 
