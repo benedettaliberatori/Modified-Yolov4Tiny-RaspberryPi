7c68a5f931307acd7b2fbba32971de740ae2f8b6
ciroantoniomami <mamiciroantonio@gmail.com>
2021-06-08 09:28:05 +0200

    Update

--- dataset.py
+++ dataset.py
@@ -101,7 +101,7 @@ def get_data(train_csv_path, test_csv_path):
     
 
     IMAGE_SIZE = 416
-    BATCH_SIZE = 8
+    BATCH_SIZE = 1
     NUM_WORKERS = 4
     DATASET = 'dataset'
     IMG_DIR = DATASET + "/images/"
