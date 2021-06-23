bff3ca6b5fbb7be5a8f0ab17f5ef1cd3f8454f00
jovoni <giovy.santa@gmail.com>
2021-06-23 12:40:28 +0200

    fps

--- detect.py
+++ detect.py
@@ -3,6 +3,7 @@ from yolo2 import Yolo
 import torch
 from torchvision import transforms
 import time
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
@@ -24,16 +25,34 @@ if __name__ == "__main__":
     
     start = time.perf_counter()
     count = 0
+
+    prev_frame_time = 0
+    new_frame_time = 0
+
+    history_fps = []
+
     while True:
         r, frame = cap.read()
         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         frame = cv.resize(frame, (416, 416))
+
+        font = cv.FONT_HERSHEY_SIMPLEX
+        new_frame_time = time.time()
+
         frame_tensor = transforms.ToTensor()(frame).unsqueeze_(0)
         
          
         frame = model.detect_Persson(frame, frame_tensor, scaled_anchors)
         frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
         count += 1
+        fps = 1/(new_frame_time-prev_frame_time)
+        history_fps.append(fps)
+        
+        prev_frame_time = new_frame_time
+        fps = "{:3.4f}".format(fps)
+        fps = "FPS: " + fps
+        cv.putText(frame, fps, (0, 30), font, 0.5, (255, 0, 0), 1, cv.LINE_AA)
+        
         cv.imshow('detecter', frame)
  
         c = cv.waitKey(1)
@@ -42,5 +61,8 @@ if __name__ == "__main__":
             cv.destroyAllWindows()
             break
     
+    plt.plot(history_fps)
+    plt.ylabel("FPS")
+    plt.show()
     end = time.perf_counter()
     print(count/(end-start))
