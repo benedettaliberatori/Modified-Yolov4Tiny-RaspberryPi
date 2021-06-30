87ed12b597e5400df8240e59fe0622b8f2a59523
benedettaliberatori <benny.libera@gmail.com>
2021-06-30 10:02:45 +0200

    '.'

--- structured_pruning.py
+++ structured_pruning.py
@@ -1,5 +1,5 @@
 import torch
-import torch_pruning as tp
+#import torch_pruning as tp
 from yolo2 import Yolo_Block
 from train2 import train_model, RAdam , test_model
 from CSP import ConvBlock, ResBlockD
@@ -12,29 +12,29 @@ from onnx_tf.backend import prepare
 import tensorflow as tf
 
 
-def prune_model(model):
-    model.cpu()
-    DG = tp.DependencyGraph().build_dependency( model, torch.randn(1, 3, 416, 416) )
-    def prune_conv(conv, amount=0.2):
-        
-        strategy = tp.strategy.L1Strategy() 
-        pruning_index = strategy(conv.weight, amount=amount)
-        plan = DG.get_pruning_plan(conv, tp.prune_conv, pruning_index)
-        plan.exec()
-    
-    
-    
-    for m in model.modules():
-        if isinstance( m, ConvBlock ):
-            prune_conv( m.conv, 0.5)
-        #if isinstance( m, ResBlockD):
-        #    prune_conv(m.conv1, 0.3)
-        #    prune_conv(m.conv2, 0.3)
-        #    prune_conv(m.conv3, 0.3)
-        #    prune_conv(m.conv4, 0.3)
-        
-            
-    return model   
+#def prune_model(model):
+#    model.cpu()
+#    DG = tp.DependencyGraph().build_dependency( model, torch.randn(1, 3, 416, 416) )
+#    def prune_conv(conv, amount=0.2):
+#        
+#        strategy = tp.strategy.L1Strategy() 
+#        pruning_index = strategy(conv.weight, amount=amount)
+#        plan = DG.get_pruning_plan(conv, tp.prune_conv, pruning_index)
+#        plan.exec()
+#    
+#    
+#    
+#    for m in model.modules():
+#        if isinstance( m, ConvBlock ):
+#            prune_conv( m.conv, 0.5)
+#        #if isinstance( m, ResBlockD):
+#        #    prune_conv(m.conv1, 0.3)
+#        #    prune_conv(m.conv2, 0.3)
+#        #    prune_conv(m.conv3, 0.3)
+#        #    prune_conv(m.conv4, 0.3)
+#        
+#            
+#    return model   
 
 
 if __name__ == '__main__':
@@ -53,39 +53,39 @@ if __name__ == '__main__':
     loss_fn = Loss()
 
 
-    model = Yolo_Block(3,3,2)
-    model.load_state_dict(torch.load('model_RAdam_Augmented.pt'))
-
-    params = sum([np.prod(p.size()) for p in model.parameters()])
-    print("Number of Parameters: %.1fM"%(params/1e6))
-    test_model(model, test_loader, scaled_anchors, performance=class_accuracy, loss_fn= Loss(), device=None)
-
-    prune_model(model)
-    params = sum([np.prod(p.size()) for p in model.parameters()])
-    print("Number of Parameters: %.1fM"%(params/1e6))
-    test_model(model, test_loader, scaled_anchors, performance=class_accuracy, loss_fn= Loss(), device=None)
-
-
-    optimizer = RAdam(model.parameters(), lr=0.001/5, weight_decay=0.005)
-    num_epochs = 100
-    scaler = torch.cuda.amp.GradScaler()
-    train_model(train_loader, model, optimizer, Loss(), num_epochs, scaler,  scaled_anchors,None, performance=class_accuracy,lr_scheduler= None,epoch_start_scheduler= 40)
-
-
-    sample_input = torch.rand((1,3,416, 416))
-    torch.onnx.export(
-    model,                  # PyTorch Model
-    sample_input,                    # Input tensor
-    'onnx_model.onnx',        # Output file (eg. 'output_model.onnx')
-    opset_version=12,       # Operator support version
-    input_names=['input'] ,  # Input tensor name (arbitary)
-    output_names=['output1', 'output2'] )
-
-    model = onnx.load("onnx_model.onnx")
-
-    tf_rep = prepare(model)
-    tf_rep.export_graph('model_tf')
-
+    #model = Yolo_Block(3,3,2)
+    #model.load_state_dict(torch.load('model_RAdam_Augmented.pt'))
+#
+    #params = sum([np.prod(p.size()) for p in model.parameters()])
+    #print("Number of Parameters: %.1fM"%(params/1e6))
+    #test_model(model, test_loader, scaled_anchors, performance=class_accuracy, loss_fn= Loss(), device=None)
+#
+    #prune_model(model)
+    #params = sum([np.prod(p.size()) for p in model.parameters()])
+    #print("Number of Parameters: %.1fM"%(params/1e6))
+    #test_model(model, test_loader, scaled_anchors, performance=class_accuracy, loss_fn= Loss(), device=None)
+#
+#
+    #optimizer = RAdam(model.parameters(), lr=0.001/5, weight_decay=0.005)
+    #num_epochs = 100
+    #scaler = torch.cuda.amp.GradScaler()
+    #train_model(train_loader, model, optimizer, Loss(), num_epochs, scaler,  scaled_anchors,None, performance=class_accuracy,lr_scheduler= None,epoch_start_scheduler= 40)
+#
+#
+    #sample_input = torch.rand((1,3,416, 416))
+    #torch.onnx.export(
+    #model,                  # PyTorch Model
+    #sample_input,                    # Input tensor
+    #'onnx_model.onnx',        # Output file (eg. 'output_model.onnx')
+    #opset_version=12,       # Operator support version
+    #input_names=['input'] ,  # Input tensor name (arbitary)
+    #output_names=['output1', 'output2'] )
+#
+    #model = onnx.load("onnx_model.onnx")
+#
+    #tf_rep = prepare(model)
+    #tf_rep.export_graph('model_tf')
+#
     converter = tf.lite.TFLiteConverter.from_saved_model('model_tf')
     tflite_model = converter.convert()
     with open('model1.tflite', 'wb') as f:
