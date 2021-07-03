48628242852e51be811751b8b3dece992d6c0e7f
benedettaliberatori <benny.libera@gmail.com>
2021-07-03 17:05:55 +0200

    v

--- utilities/structured_pruning.py
+++ utilities/structured_pruning.py
@@ -17,15 +17,19 @@ import tensorflow as tf
 
 
 def prune_model(model):
+    
     model.cpu()
+    
+    # forward the model with a fake input to 
+    # compute a dependency graph
     DG = tp.DependencyGraph().build_dependency( model, torch.randn(1, 3, 416, 416) )
+    
     def prune_conv(conv, amount=0.2):
         
-        strategy = tp.strategy.L1Strategy() 
+        strategy = tp.strategy.L1Strategy() # setup strategy 
         pruning_index = strategy(conv.weight, amount=amount)
         plan = DG.get_pruning_plan(conv, tp.prune_conv, pruning_index)
-        plan.exec()
-    
+        plan.exec() 
     
     
     for m in model.modules():
@@ -69,16 +73,17 @@ if __name__ == '__main__':
     optimizer = RAdam(model.parameters(), lr=0.001/5, weight_decay=0.005)
     num_epochs = 50
     scaler = torch.cuda.amp.GradScaler()
+    # Retrain the model
     train_model(train_loader, model, optimizer, Loss(), num_epochs, scaler,  scaled_anchors,None, performance=class_accuracy,lr_scheduler= None,epoch_start_scheduler= 40)
 
-
+    # Conversion PyTorch-ONNX-TF-TFlite
     sample_input = torch.rand((1,3,416, 416)).to("cuda:0")
     torch.onnx.export(
-    model,                  # PyTorch Model
-    sample_input,                    # Input tensor
-    'onnx_model.onnx',        # Output file (eg. 'output_model.onnx')
-    opset_version=12,       # Operator support version
-    input_names=['input'] ,  # Input tensor name (arbitary)
+    model,                  
+    sample_input,                   
+    'onnx_model.onnx',       
+    opset_version=12,       
+    input_names=['input'] ,  
     output_names=['output1', 'output2'] )
 
     model = onnx.load("onnx_model.onnx")
@@ -86,9 +91,9 @@ if __name__ == '__main__':
     tf_rep = prepare(model)
     tf_rep.export_graph('modeld50_tf')
 
-    #converter = tf.lite.TFLiteConverter.from_saved_model('model_tf')
-    #tflite_model = converter.convert()
-    #with open('prunded.tflite', 'wb') as f:
-    #    f.write(tflite_model)
+    converter = tf.lite.TFLiteConverter.from_saved_model('model_tf')
+    tflite_model = converter.convert()
+    with open('prunded.tflite', 'wb') as f:
+        f.write(tflite_model)
 
     
