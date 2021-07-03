0ee05ea76c048a69ef7bfc91c8c75899d6d2a3ac
benedettaliberatori <benny.libera@gmail.com>
2021-07-03 11:52:55 +0200

    kk2

--- utils/structured_pruning.py
+++ utils/structured_pruning.py
@@ -3,8 +3,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #sys.path.append("..")
 
 import torch
-#import torch_pruning as tp
-#sys.path.insert(0, str(pathlib.Path(__file__).parent))
+import torch_pruning as tp
 from yolo.yolo2 import Yolo_Block
 
 
